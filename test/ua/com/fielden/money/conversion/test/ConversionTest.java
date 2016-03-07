a4aa81ac1ac8f1c8c2cf7886edb95f6af85a14d7
krvavych <natalia.krvavych@gmail.com>
2016-03-07 16:38:45 +0200

    Added new tests.

--- test/ua/com/fielden/money/conversion/test/ConversionTest.java
+++ test/ua/com/fielden/money/conversion/test/ConversionTest.java
@@ -1,6 +1,7 @@
 package ua.com.fielden.money.conversion.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.math.BigDecimal;
@@ -46,7 +47,6 @@ public class ConversionTest {
         assertEquals("тридцять шість гривень ", new Converter().convertNumbersToWriting(new Money(new BigDecimal("36.00"))));
         assertEquals("одна тисяча сто дві гривні одна копійка", new Converter().convertNumbersToWriting(new Money(new BigDecimal("1102.01"))));
         assertEquals("двадцять три копійки", new Converter().convertNumbersToWriting(new Money(new BigDecimal("0.23"))));
-        assertEquals("", new Converter().convertNumbersToWriting(new Money(new BigDecimal(""))));
     }
 
     @Test
@@ -54,5 +54,33 @@ public class ConversionTest {
         final Money money1 = new Money(new BigDecimal("19"));
         final Money money2 = new Money(new BigDecimal("19.00"));
         assertEquals(new Converter().convertNumbersToWriting(money1), new Converter().convertNumbersToWriting(money2));
+    }
+
+    @Test
+    public void nouns_should_be_used_correct () throws IllegalAccessException{
+        final Money money1 = new Money(new BigDecimal("191"));
+        final Money money2 = new Money(new BigDecimal("102000"));
+        final Money money3 = new Money(new BigDecimal("19.05"));
+        assertTrue(new Converter().convertNumbersToWriting(money1).contains("гривня"));
+        assertTrue(new Converter().convertNumbersToWriting(money2).contains("тисячі"));
+        assertTrue(new Converter().convertNumbersToWriting(money3).contains("копійок"));
+    }
+
+    @Test
+    public void  numerals_should_be_used_correct () throws IllegalAccessException{
+        final Money money1 = new Money(new BigDecimal("101"));
+        final Money money2 = new Money(new BigDecimal("11"));
+        final Money money3 = new Money(new BigDecimal("22"));
+        final Money money4 = new Money(new BigDecimal("12"));
+        final Money money5 = new Money(new BigDecimal("09"));
+        final Money money6 = new Money(new BigDecimal("90"));
+        final Money money7 = new Money(new BigDecimal("0010"));
+        assertTrue(new Converter().convertNumbersToWriting(money1).contains("одна"));
+        assertTrue(new Converter().convertNumbersToWriting(money2).contains("одинадцять"));
+        assertTrue(new Converter().convertNumbersToWriting(money3).contains("дві"));
+        assertTrue(new Converter().convertNumbersToWriting(money4).contains("дванадцять"));
+        assertTrue(new Converter().convertNumbersToWriting(money5).contains("дев’ять"));
+        assertTrue(new Converter().convertNumbersToWriting(money6).contains("дев’яносто"));
+        assertTrue(new Converter().convertNumbersToWriting(money7).contains("десять"));
     }
 }
