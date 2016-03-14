f306818b4c9eb608264d479f38ec9fbd5f9cb4f1
krvavych <natalia.krvavych@gmail.com>
2016-03-14 15:34:58 +0200

    Remove isEqual function

--- src/ua/com/fielden/money/conversion/Converter.java
+++ src/ua/com/fielden/money/conversion/Converter.java
@@ -14,7 +14,7 @@ public class Converter {
     public String convertNumbersToWriting(final Money digitalRepresentation) throws IllegalAccessException {
         final String wholePart = digitalRepresentation.getWholePart();
         final String fractionalPart = digitalRepresentation.getFractionalPart();
-        if (isEqual(wholePart.substring(0, 1), "-")) {
+        if (wholePart.substring(0, 1).equals("-")) {
             throw new IllegalArgumentException("Money should be positive!");
         }
         return convert(wholePart, fractionalPart);
@@ -25,7 +25,7 @@ public class Converter {
         final String firstEnd = (wholePart.length() > 3) ? (chooseVariant(wholePart.substring(0, wholePart.length() - 3), thousend)) : "";
         final String secondEnd = (wholePart.length() < 4) ? chooseVariant(wholePart, hryvnia)
                 : (chooseVariant(wholePart.substring((wholePart.length() - 3), wholePart.length()), hryvnia));
-        final String thirdEnd = (!isEqual(fractionalPart, "00") && !isEqual(fractionalPart, "")) ? chooseVariant(fractionalPart, kop) : "";
+        final String thirdEnd = (!(fractionalPart.equals("00")) && !(fractionalPart.equals(""))) ? chooseVariant(fractionalPart, kop) : "";
 
         String fractionalPartConverted = "";
         if (fractionalPart != "") {
@@ -56,13 +56,13 @@ public class Converter {
 
     }
 
-    private boolean isEqual(final String elem1, final String elem2) {
-        if (elem1.hashCode() == elem2.hashCode()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+//    private boolean isEqual(final String elem1, final String elem2) {
+//        if (elem1.equals(elem2)) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
 
     private String chooseVariant(final String numeric, final String[] array) {
         if (numeric.length() == 1) {
@@ -70,14 +70,14 @@ public class Converter {
         } else if (numeric.length() == 2) {
             final String firstDigit = numeric.substring(0, 1);
             final String secondDigit = numeric.substring(1);
-            if (isEqual(firstDigit, "1") || (!isEqual(firstDigit, "1") && !isEqual(firstDigit, "0") && isEqual(secondDigit, "0"))) {
+            if ((firstDigit.equals("1")) || (!(firstDigit.equals("1")) && !(firstDigit.equals("0")) && (secondDigit.equals("0")))) {
                 return array[2];
             } else {
                 return checkDigit(secondDigit, array);
             }
         } else {
             final String thirdDigit = numeric.substring(2);
-            if (isEqual(thirdDigit, "0")) {
+            if (thirdDigit.equals("0")) {
                 return array[2];
             } else {
                 return checkDigit(thirdDigit, array);
@@ -86,7 +86,7 @@ public class Converter {
     }
 
     private String checkDigit(final String digit, final String[] array) {
-        return ((isEqual(digit, "1") ? array[0] : (isEqual(digit, "2") || isEqual(digit, "3") || isEqual(digit, "4") ? array[1] : array[2])));
+        return (((digit.equals("1")) ? array[0] : ((digit.equals("2")) || (digit.equals("3")) || (digit.equals("4")) ? array[1] : array[2])));
     }
 
     private String convertDigits(final String numeric, final String[] array) {
@@ -101,15 +101,15 @@ public class Converter {
     private String convertDecades(final String numeric) {
         final String firstDigit = numeric.substring(0, 1);
         final String secondDigit = numeric.substring(1);
-        if (isEqual(firstDigit, "1") && !isEqual(secondDigit, "0")) {
+        if ((firstDigit.equals("1")) && !(secondDigit.equals("0"))) {
             return convertDigits(secondDigit, firstDecade);
         }
         for (int i = 0; i < decades.length; i++) {
-            if (isEqual(firstDigit, "0")) {
+            if ((firstDigit.equals("0"))) {
                 return convertDigits(secondDigit, digits);
-            } else if ((isEqual(firstDigit, ((i + 1) + ""))) && (isEqual(secondDigit, "0"))) {
+            } else if (((firstDigit.equals((i + 1) + ""))) && ((secondDigit.equals("0")))) {
                 return decades[i] + " ";
-            } else if ((isEqual(firstDigit, ((i + 1) + ""))) && (!isEqual(secondDigit, "0"))) {
+            } else if (((firstDigit.equals(((i + 1) + "")))) && (!(secondDigit.equals("0")))) {
                 return decades[i] + " " + convertDigits(secondDigit, digits);
             }
         }
@@ -122,13 +122,13 @@ public class Converter {
         final String thirdDigit = numeric.substring(2);
 
         for (int i = 0; i < hundreds.length; i++) {
-            if (isEqual(firstDigit, "0")) {
+            if (firstDigit.equals("0")) {
                 return convertDecades(secondDigit + thirdDigit);
-            } else if (isEqual(firstDigit, ((i + 1) + "")) && isEqual(secondDigit, "0") && isEqual(thirdDigit, "0")) {
+            } else if (firstDigit.equals(((i + 1) + "")) && (secondDigit.equals("0")) && (thirdDigit.equals("0"))) {
                 return hundreds[i] + " ";
-            } else if (isEqual(firstDigit, ((i + 1) + "")) && isEqual(secondDigit, "0") && !isEqual(thirdDigit, "0")) {
+            } else if ((firstDigit.equals(((i + 1) + ""))) && (secondDigit.equals("0")) && !(thirdDigit.equals("0"))) {
                 return hundreds[i] + " " + convertDigits(thirdDigit, digits);
-            } else if (isEqual(firstDigit, ((i + 1) + "")) && !isEqual(secondDigit, "0")) {
+            } else if ((firstDigit.equals(((i + 1) + ""))) && !((secondDigit.equals("0")))) {
                 return hundreds[i] + " " + convertDecades(secondDigit + thirdDigit);
             }
         }
